d8231f8c6560cfc210627832ba44c959c6f934dc
Jack Dockerty <jdockerty19@gmail.com>
2020-03-14 14:09:30 +0000

    Testing command execution.

--- main.go
+++ main.go
@@ -1,18 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"net/http"
+	"os/exec"
 )
 
-func HealthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Status: %d", http.StatusOK)
+type Client struct {
+	ClientName string
+	TLSEncrypt string
+}
+
+type Status struct {
+	Code int
+}
+
+// StatusHandler used to check whether the server is running, the 200 OK is returned if it is.
+// This provides an easy way to check whether the service is running before constructing a larger POST.
+func StatusHandler(w http.ResponseWriter, r *http.Request) {
+	newStatusResponse := Status{Code: http.StatusOK}
+	json.Marshal(newStatusResponse)
+	json.NewEncoder(w).Encode(newStatusResponse)
+}
+
+// AddClientHandler is used to add a client to the running OpenVPN server.
+// It uses the JSON contained within the POST request method to construct a profile on the server. This responds with the constructed profile to insert
+// into the OpenVPN Client UI.
+func AddClientHandler(w http.ResponseWriter, r *http.Request) {
+	var newClient Client
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(requestBody, &newClient)
+
+	exec.Command("sudo openvpn-install/openvpn-install.sh")
+	exec.Command("1")
+	exec.Command(newClient.ClientName)
+	TLSCommandString := "sudo ls /root/" + newClient.ClientName + ".ovpn"
+	
+	output, err := exec.Command(TLSCommandString).Output()
+	if err != nil {
+		panic(err)
+	}
+	newClient = Client{ClientName : output}
+
+	fmt.Println("New Client:", newClient)
+	json.NewEncoder(w).Encode("New Client:" + newClient.ClientName)
 }
 
 func main() {
 	newRouter := mux.NewRouter()
-
-	newRouter.HandleFunc("/", HealthHandler)
+	fmt.Println("Running...")
+	newRouter.HandleFunc("/api/Status", StatusHandler)
+	newRouter.HandleFunc("/api/AddClient", AddClientHandler).Methods("POST")
 	http.ListenAndServe(":8080", newRouter)
 }
